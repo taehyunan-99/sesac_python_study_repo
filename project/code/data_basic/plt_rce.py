31c0c0761132811831e4befb43e59ba433c17395
taehyunan-99 <withblua@gmail.com>
2026-01-12 17:28:49 +0900

    260112_update

---
+++ project/code/data_basic/plt_rce.py
@@ -0,0 +1,7 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.rc(group='font', family='Gowun Batang', size=10)
+plt.rc(group='figure', figsize=(8, 4), dpi=120)
+plt.rc(group='axes', unicode_minus=False)
+plt.rc(group='legend', frameon=True, fc='0.9', ec='0.9')
